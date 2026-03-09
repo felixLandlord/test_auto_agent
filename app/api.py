90f944fdee9f248889cde74c4e7b87ef0f4cc198
felixLandlord <felix.landlord.nunoo@gmail.com>
2026-03-09 11:13:05 +0000

    update to route prefix - removal

--- app/api.py
+++ app/api.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from app.service import CalculatorService
 
-router = APIRouter(prefix="/calculator")
+router = APIRouter()
 
 class CalculationInput(BaseModel):
     a: float
